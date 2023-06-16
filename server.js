c6d7edb19c82b20d6d776daca586c75c9ab5c5ab
jeniljain2811 <jeniljain2811@gmail.com>
2023-06-16 16:41:41 -0700

    added cart and auth pages

--- server.js
+++ server.js
@@ -9,15 +9,27 @@ const PORT = process.env.PORT || 3003
 //Ststic Assets
 app.use(express.static('public'))
 
-app.get('/' , (req,res)=>{
-    res.render('home') //now you have to set path after views folder as you had set(app.set) that take layout files from views folder & now you just have to specify which file to take when
-})
-
 //Set Template Engine
 app.use(expressLayouts)
 //Set the layout files folder
 app.set('views' , path.join(__dirname , '/resources/views'))
 app.set('view engine' , 'ejs')
+
+app.get('/' , (req,res)=>{
+    res.render('home') //now you have to set path after views folder as you had set(app.set) that take layout files from views folder & now you just have to specify which file to take when
+})
+
+app.get('/cart' , (req,res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login' , (req,res)=>{
+    res.render('auth/login')
+})
+
+app.get('/register' , (req,res)=>{
+    res.render('auth/register')
+})
 
 app.listen(PORT , ()=>{
     console.log(`Server on port ${PORT}`);
